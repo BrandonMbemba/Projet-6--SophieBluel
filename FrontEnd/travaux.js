f46baf88a07d84dd91bb5f5d3fd2b462dfd46653
Brandon M'bemba <brandonmbemba@gmail.com>
2024-01-10 17:01:32 +0100

    projects within categorys v1

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,3 +1,6 @@
+// Travaux //
+
+
 const travauxContainer = document.querySelector('.gallery')
 
 let travauxApi = await fetch ('http://localhost:5678/api/works')
@@ -20,12 +23,36 @@ function createWorks (travauxApi, gallery) {
 
 }
 
-// affichage travaux par filtres //
+// Filtres //
 
+const filtersContainer = document.getElementById("portfolio")
+
+// Récupération des catégories
 async function getCategory () {
     let categoryApi = await fetch ('http://localhost:5678/api/categories');
-    categoryApi = await categoryApi.json();
-    console.log(categoryApi)
+    return await categoryApi.json();
 }
 
 getCategory();
+
+// Création des catégories //
+
+async function createCategory () {
+    const categorys = await getCategory();
+    categorys.forEach((category) => {
+        filtersContainer.innerHTML += `<button id ="${category.id}">${category.name}</button>`
+    });
+}
+
+createCategory();
+
+// Filtrer les travaux par catégories //
+
+async function worksWithinCategory() {
+    const travaux = await travauxApi
+    console.log(travaux);
+    const button = document.querySelectorAll("portfolio button")
+    console.log(button)
+}
+
+worksWithinCategory();
