913ee921b940e7755df8e6fbe212323b38c444db
Brandon M'bemba <brandonmbemba@gmail.com>
2024-02-11 14:48:18 +0100

    final commit v2 (checking if file is an image in form.submit)

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,288 +3,309 @@
 
 const travauxContainer = document.querySelector('.gallery')
 
-let travauxApi = await fetch ('http://localhost:5678/api/works')
+let travauxApi = await fetch('http://localhost:5678/api/works')
 travauxApi = await travauxApi.json()
 
-createWorks(travauxApi,travauxContainer)
+createWorks(travauxApi, travauxContainer)
 
-function createWorks (travaux, gallery) {
-    gallery.innerHTML= ""
-    travaux.forEach(element => {
-        gallery.innerHTML += `<figure> <img src="${element.imageUrl}" alt="" /> ${element.title}
-         </figure>`      
-    });
-    deleteProjects();
+function createWorks(travaux, gallery) {
+  gallery.innerHTML = ''
+  travaux.forEach(element => {
+    gallery.innerHTML += `<figure> <img src="${ element.imageUrl }" alt="" /> ${ element.title }
+         </figure>`
+  })
+  deleteProjects()
 }
 
 // Filtres //
 
 const filtersContainer = document.querySelector('.filters')
 
-async function getCategories () {
-    const categoryApi = await fetch ('http://localhost:5678/api/categories');
-    return await categoryApi.json();
+async function getCategories() {
+  const categoryApi = await fetch('http://localhost:5678/api/categories')
+  return await categoryApi.json()
 }
 
 
-async function createCategories () {
-    const categories = await getCategories();
-    categories.forEach(category => {
-        filtersContainer.innerHTML += `<button class = "filter__btn" id ="${category.id}">${category.name}</button>`
-        
-    });
+async function createCategories() {
+  const categories = await getCategories()
+  categories.forEach(category => {
+    filtersContainer.innerHTML += `<button class = "filter__btn" id ="${ category.id }">${ category.name }</button>`
+
+  })
 }
 
-await createCategories();
+await createCategories()
 
 
-async function filtersCategories() {
-const works = await travauxApi;
-const buttons = document.querySelectorAll(".filter__btn");
-buttons.forEach(button => {
-    button.addEventListener("click",(e)=> {
-        const buttonId = e.target.id;
-        travauxContainer.innerHTML = "";
-        if (buttonId !== "null") {
-            const worksFilters = works.filter(travail => {
-                return travail.categoryId == buttonId;
-            });
-           createWorks(worksFilters,travauxContainer);
-        } else {
-            createWorks(travauxApi,travauxContainer);
-        }
+function filtersCategories() {
+  const works = travauxApi
+  const buttons = document.querySelectorAll('.filter__btn')
+  buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const buttonId = e.target.id
+      travauxContainer.innerHTML = ''
+      if (buttonId !== 'null') {
+        const worksFilters = works.filter(travail => {
+          return travail.categoryId == buttonId
         })
+        createWorks(worksFilters, travauxContainer)
+      } else {
+        createWorks(travauxApi, travauxContainer)
+      }
     })
-};
+  })
+}
 
-await filtersCategories();
+filtersCategories()
 
 //Changement couleur au clic //
 
-const buttons = document.querySelectorAll(".filter__btn");
-buttons.forEach(button => button.addEventListener("click", function(event) {
-    buttons.forEach((bouton) => {
-        console.log(bouton)
-        bouton.classList.remove("filter__btn--active");
-    });
-    console.log(button.id);
-    event.target.classList.add("filter__btn--active"); 
+const buttons = document.querySelectorAll('.filter__btn')
+buttons.forEach(button => button.addEventListener('click', function (event) {
+  buttons.forEach((bouton) => {
+    bouton.classList.remove('filter__btn--active')
+  })
+  event.target.classList.add('filter__btn--active')
 }))
 
 
 // Quand l'utilisateur est connecté //
 
-const loged = window.sessionStorage.Connecté;
-const token = window.sessionStorage.Token;
-const admin = document.querySelector(".admin");
-const logout = document.querySelector(".logout");
-const edit = document.querySelector(".edit");
-const modaleContainer = document.querySelector(".modale__container");
-const modaleGalery = document.querySelector(".modale");
-const modaleButton = document.querySelector(".modale__button");
-const modaleButton2 = document.querySelector(".modale__2 button");
-const modaleAdd = document.querySelector(".modale__2");
-const x = document.querySelector(".modale__container .fa-xmark");
-const modaleAddXbutton = document.querySelector(".modale__2 .fa-xmark");
-const modale = document.querySelector(".modale__photos");
-const returnArrow = document.querySelector(".modale__container .fa-arrow-left");
-const inputs = document.querySelectorAll("input");
+const loged = window.sessionStorage.Connecte
+const token = window.sessionStorage.Token
+const admin = document.querySelector('.admin')
+const logout = document.querySelector('.logout')
+const edit = document.querySelector('.edit')
+const modaleContainer = document.querySelector('.modale__container')
+const modaleGalery = document.querySelector('.modale')
+const modaleButton = document.querySelector('.modale__button')
+const modaleButton2 = document.querySelector('.modale__2 button')
+const modaleAdd = document.querySelector('.modale__2')
+const modaleX = document.querySelector('.modale__container .fa-xmark')
+const modaleAddXbutton = document.querySelector('.modale__2 .fa-xmark')
+const modale = document.querySelector('.modale__photos')
+const returnArrow = document.querySelector('.modale__container .fa-arrow-left')
 
-if (loged == "true") {
-    admin.classList.remove("admin");
-    logout.innerHTML = "logout";
-    logout.classList.add("text-size");
-    edit.style.display = "flex";
-    filtersContainer.classList.add("admin");
-    logout.addEventListener("click", ()=>{
-        window.sessionStorage.Connecté = false
-    })
+if (loged == 'true') {
+  admin.classList.remove('admin')
+  logout.innerHTML = 'logout'
+  logout.classList.add('text-size')
+  edit.style.display = 'flex'
+  filtersContainer.classList.add('admin')
+  logout.addEventListener('click', () => {
+    window.sessionStorage.Connecte = false
+  })
 }
 
-function firstModaleDisplay () {
-    edit.addEventListener("click", () => {
-        modaleContainer.style.display = "flex";
-        modaleAdd.style.display = null;
-        edit.style.display = null;
-    })
+function firstModaleDisplay() {
+  edit.addEventListener('click', () => {
+    modaleContainer.style.display = 'flex'
+    modaleAdd.style.display = null
+    edit.style.display = null
+  })
 
-    x.addEventListener("click", () => {
-        modaleContainer.style.display = null;
-        edit.style.display = "flex";
-    })
+  modaleX.addEventListener('click', () => {
+    modaleContainer.style.display = null
+    edit.style.display = 'flex'
+  })
 
 
-    modaleContainer.addEventListener("click", (e) => {
-        if(e.target.className == "modale__container") {
-            modaleContainer.style.display = null;
-            edit.style.display = "flex";
-        }
-    })
+  modaleContainer.addEventListener('click', (e) => {
+    if (e.target.className == 'modale__container') {
+      modaleContainer.style.display = null
+      edit.style.display = 'flex'
+      modaleButton2.classList.remove('modale__button')
+      modaleButton2.classList.add('button')
+      reset()
+    }
+  })
 }
 
-firstModaleDisplay();
+firstModaleDisplay()
 
 
 // Affichage projets dans modale 
 
 async function displayProjectsModale() {
-    modale.innerHTML = ""
-    const projects = await travauxApi;
-    projects.forEach(travail => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const span = document.createElement("span");
-        const trash = document.createElement("i");
-        trash.classList.add("fa-solid", "fa-trash-can");
-        trash.id = travail.id;
-        img.src = travail.imageUrl;
-        span.appendChild(trash);
-        figure.appendChild(span);
-        figure.appendChild(img);
-        modale.appendChild(figure);
-    });
-    deleteProjects();
+  modale.innerHTML = ''
+  const projects = await travauxApi
+  projects.forEach(travail => {
+    const figure = document.createElement('figure')
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+    const trash = document.createElement('i')
+    trash.classList.add('fa-solid', 'fa-trash-can')
+    trash.id = travail.id
+    img.src = travail.imageUrl
+    span.appendChild(trash)
+    figure.appendChild(span)
+    figure.appendChild(img)
+    modale.appendChild(figure)
+  })
+  deleteProjects()
 
 }
 
-await displayProjectsModale(); 
+await displayProjectsModale()
 
 async function deleteProjects() {
-    const trashes = document.querySelectorAll(".fa-trash-can");
-    trashes.forEach(async (trash) =>  {
-        const id = parseInt(trash.id,10);
-        trash.addEventListener("click", async (e) => {
-            e.preventDefault();
-            const response = await fetch (`http://localhost:5678/api/works/${id}`,{
-            method: "DELETE",
-            headers: {
-                Authorization : `Bearer ${token}`,
-            }
-        })
-             if (response.ok) {
-                travauxApi = travauxApi.filter((travail) => (id !== travail.id))
-                createWorks(travauxApi,travauxContainer);
-                displayProjectsModale();
-            } else {
-            alert("Echec de suppression");
-            }
-        })    
-    });
-   
-};
+  const trashes = document.querySelectorAll('.fa-trash-can')
+  trashes.forEach(async (trash) => {
+    const id = parseInt(trash.id, 10)
+    trash.addEventListener('click', async (e) => {
+      e.preventDefault()
+      const response = await fetch(`http://localhost:5678/api/works/${ id }`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${ token }`,
+        }
+      })
+      if (response.ok) {
+        travauxApi = travauxApi.filter((travail) => (id !== travail.id))
+        createWorks(travauxApi, travauxContainer)
+        displayProjectsModale()
+      } else {
+        alert('Echec de suppression')
+      }
+    })
+  })
+
+}
 
 // Affichage 2e modale 
 
-function secondModaleDisplay () {
-    modaleButton.addEventListener("click", () => {
-        modaleGalery.style.display = "none"
-        modaleAdd.style.display = "flex"
-    })
+function secondModaleDisplay() {
+  modaleButton.addEventListener('click', () => {
+    modaleGalery.style.display = 'none'
+    modaleAdd.style.display = 'flex'
+  })
 
-    returnArrow.addEventListener("click", () => {
-        modaleGalery.style.display = "flex"
-        modaleAdd.style.display = "none"
-    })
+  returnArrow.addEventListener('click', () => {
+    modaleGalery.style.display = 'flex'
+    modaleAdd.style.display = 'none'
+    modaleButton2.classList.remove('modale__button')
+    modaleButton2.classList.add('button')
+    reset();
+  })
 
-    modaleAddXbutton.addEventListener("click", () => {
-        modaleContainer.style.display = null;
-        edit.style.display = "flex";
-    })
+  modaleAddXbutton.addEventListener('click', () => {
+    modaleContainer.style.display = null
+    edit.style.display = 'flex'
+    modaleButton2.classList.remove('modale__button')
+    modaleButton2.classList.add('button')
+    reset();
+  })
 
-    edit.addEventListener("click", () => {
-        modaleContainer.style.display = "flex";
-        modaleGalery.style.display = "flex"
-        edit.style.display = null;
-    })
+  edit.addEventListener('click', () => {
+    modaleContainer.style.display = 'flex'
+    modaleGalery.style.display = 'flex'
+    edit.style.display = null
+  })
 
 }
 
-secondModaleDisplay();
+secondModaleDisplay()
 
-function reset (){
-    form.reset();
-    imagePreview.style.display = "none";
-    imageLabel.style.display = "flex";
-    imageLogo.style.display = "flex";
+function changeButtonColor() {
+  modaleButton2.classList.remove('button')
+  modaleButton2.classList.add('modale__button')
+}
+
+function reset() {
+  form.reset()
+  imagePreview.style.display = 'none'
+  imageLabel.style.display = 'flex'
+  imageLogo.style.display = 'flex'
 }
 
 function backToModal() {
-    modaleContainer.style.display = "flex";
-    modaleGalery.style.display = "flex"
-    modaleAdd.style.display = null
-    edit.style.display = null;
+  modaleContainer.style.display = 'flex'
+  modaleGalery.style.display = 'flex'
+  modaleAdd.style.display = null
+  edit.style.display = null
 }
 
-const imageInput = document.getElementById("file")
-const imageLabel = document.querySelector(".form-group-photo label")
-const imagePreview = document.querySelector(".form-group-photo img")
-const imageLogo = document.querySelector(".form-group-photo span")
+const imageInput = document.getElementById('file')
+const imageLabel = document.querySelector('.form-group-photo label')
+const imagePreview = document.querySelector('.form-group-photo img')
+const imageLogo = document.querySelector('.form-group-photo span')
+
 
 const previewImage = () => {
-    const file = imageInput.files;
-    if (file) {
-        const fileReader = new FileReader();
-        const preview = document.getElementById('file-preview');
-        fileReader.onload = () => {
-            imagePreview.style.display = "flex";
-            preview.setAttribute('src', event.target.result);
-        }
-        fileReader.readAsDataURL(file[0]);
-        imageLabel.style.display = "none";
-        imageLogo.style.display = "none";
-    } 
+  const file = imageInput.files
+  if (file) {
+    const fileReader = new FileReader()
+    const preview = document.getElementById('file-preview')
+    fileReader.onload = () => {
+      imagePreview.style.display = 'flex'
+      preview.setAttribute('src', event.target.result)
+    }
+    fileReader.readAsDataURL(file[ 0 ])
+    imageLabel.style.display = 'none'
+    imageLogo.style.display = 'none'
+    changeButtonColor()
+    
+  }
 }
-imageInput.addEventListener("change", previewImage);
+imageInput.addEventListener('change', previewImage)
 
 // Ajouter un projet
 
-const form = document.querySelector(".modale__2 form");
-const title = document.querySelector("#title");
-const formCategory = document.querySelector("#category");
-let allowedExtension = ['image/jpeg', 'image/jpg', 'image/png','image/gif','image/bmp'];
+const form = document.querySelector('.modale__2 form')
+const title = document.querySelector('#title')
+const formCategory = document.querySelector('#category')
+let allowedExtension = [ 'image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/bmp' ]
 
 
-form.addEventListener("submit", async (e) => {
-    let type = imageInput.files[0].type;
-    console.log(type);
-    e.preventDefault();
-    if(allowedExtension.indexOf(type)>-1)
-    {
-         alert('ok')
-        
+form.addEventListener('submit', async (e) => {
+  let type = imageInput.files[ 0 ].type
+  e.preventDefault()
+  if (allowedExtension.indexOf(type) > -1) {
+    alert('ok')
+
     const formData = new FormData()
-    formData.append("title", title.value)
-    formData.append("image", imageInput.files[0])
-    formData.append("category", formCategory.value)
-    const response = await fetch ("http://localhost:5678/api/works", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        body: formData,
-    });
-    
-    if (response.status === 201) {
-        let image = await response.json()
-        travauxApi.push(image);
-        backToModal();
-        reset();
-        displayProjectsModale();
-        createWorks(travauxApi,travauxContainer);
+    formData.append('title', title.value)
+    formData.append('image', imageInput.files[ 0 ])
+    formData.append('category', formCategory.value)
+    const response = await fetch('http://localhost:5678/api/works', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${ token }`,
+      },
+      body: formData,
+    })
 
-        alert("Projet ajouté avec succès :)");
-        
+    if (response.status === 201) {
+      let image = await response.json()
+      travauxApi.push(image)
+      backToModal()
+      reset()
+      displayProjectsModale()
+      createWorks(travauxApi, travauxContainer)
+
+      alert('Projet ajouté avec succès :)')
+
     } else if (response.status === 500) {
-        alert("Erreur inattendu");
-    
+      alert('Erreur inattendu')
+      modaleButton2.classList.remove('modale__button')
+      modaleButton2.classList.add('button')
+      reset()
+
     } else if (response.status === 400) {
-        alert("Merci de remplir tous les champs")
+      alert('Merci de remplir tous les champs')
+      modaleButton2.classList.remove('modale__button')
+      modaleButton2.classList.add('button')
+      reset()
 
     } else if (response.status === 401) {
-        alert("Vous n'êtes pas authorisé à ajouter a un projet");
-        window.location.href = "login.html";
+      alert('Vous n\'êtes pas authorisé à ajouter a un projet')
+      window.location.href = 'login.html'
     }
-    } else {
-        alert('Not a image')
-    }
+  } else {
+    alert('Not a image')
+    modaleButton2.classList.remove('modale__button')
+    modaleButton2.classList.add('button')
+    reset()
+  }
 })
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,42 +1,41 @@
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submit = document.getElementById("submit");
-const pwdEmailError = document.querySelector(".emailPassword__error");
-const form = document.querySelector("form");
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const submit = document.getElementById('submit')
+const pwdEmailError = document.querySelector('.emailPassword__error')
+const form = document.querySelector('form')
 
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-    login(e);
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  login(e)
 })
 
 
-async function login (e) {
-    const user = {
-        password: e.target.password.value,
-        email: e.target.email.value,
-    };
-    console.log(user.password,user.email);
-    const chargeUtile = JSON.stringify(user);
-    console.log(chargeUtile);
-    try{
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers:{  "Content-Type": "application/json",
-                    "accept" : "application/json",    },
-            body:   chargeUtile
+async function login(e) {
+  const user = {
+    password: e.target.password.value,
+    email: e.target.email.value,
+  }
+  const chargeUtile = JSON.stringify(user)
+  try {
+    const response = await fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json',
+      },
+      body: chargeUtile
     })
-    const data = await response.json();
-    console.log(data);
-    sessionStorage.setItem("Token", data.token);
+    const data = await response.json()
+    sessionStorage.setItem('Token', data.token)
     if (data.message || data.error) {
-        pwdEmailError.innerHTML = `<p>La combinaison e-mail/mot de passe est incorrecte<p>`
+      pwdEmailError.innerHTML = `<p>La combinaison e-mail/mot de passe est incorrecte<p>`
     } else {
-        sessionStorage.setItem("Connecté", JSON.stringify(true));
-        window.location.replace("index.html");
+      sessionStorage.setItem('Connecte', JSON.stringify(true))
+      window.location.replace('index.html')
     }
-}   catch(error) {
-    const p = document.createElement("p");
-    p.innerHTML = "La combinaison e-mail/mot de passe est incorrecte";
-    pwdEmailError.appendChild(p);
-}
+  } catch (error) {
+    const p = document.createElement('p')
+    p.innerHTML = 'La combinaison e-mail/mot de passe est incorrecte'
+    pwdEmailError.appendChild(p)
+  }
 }
